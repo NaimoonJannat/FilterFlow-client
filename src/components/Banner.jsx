b37ee0861ab886856c8e7304cce3dad4689187ba
NaimoonJannat <naimoonjannat@gmail.com>
2024-08-16 23:51:40 +0600

    Home done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,54 +1,24 @@
 import { Typewriter } from "react-simple-typewriter";
 
-
 const Banner = () => {
     return (
-        <div className="carousel w-full">
-
-            {/* first slide  */}
-  <div id="slide1" className="carousel-item relative w-full">
-    <div className='relative w-full'>
-    <img src="banner.png" className="w-full h-2/3" />
-    <div className="absolute top-0 left-0 w-full h-2/3 bg-black opacity-50"></div>
-    </div>
-    <div className="absolute top-1/4 left-10">
-            <h2 className="text-white text-center text-3xl md:text-5xl font-semibold">
-              Explore With Us
-            </h2>
-            <h2 className='text-blue-400 text-center text-4xl md:text-7xl font-bold'>
-            <Typewriter words={['Eiffel Tower','France']} loop={true} cursor={true} />
-            </h2>
-          </div>
-    <div className="absolute flex transform -translate-y-1/2 left-1/2 top-2/3">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-
-  {/* second slide  */}
-  <div id="slide2" className="carousel-item relative w-full">
-  <div className='relative w-full'>
-    <img src="banner2.png" className="w-full h-2/3" />
-    <div className="absolute top-0 left-0 w-full h-2/3 bg-black opacity-50"></div>
-    </div>
-        {/* Typewriter  */}
-        <div className="absolute top-1/4 left-10">
-            <h2 className="text-white text-center text-3xl md:text-5xl font-semibold">
-              Explore With Us
-            </h2>
-            <h2 className='text-blue-400 text-center text-4xl md:text-7xl font-bold'>
-            <Typewriter words={['Louvre Museum','France']} loop={true} cursor={true} />
-            </h2>
-          </div>
-
-    <div className="absolute flex transform -translate-y-1/2 left-1/2 top-2/3">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-
- 
-</div>
+        <div className="carousel w-full rounded-3xl">
+            {/* first slide */}
+            <div id="slide1" className="carousel-item relative w-full rounded-3xl">
+                <div className='relative w-full'>
+                    <img src="banner.png" className="w-full h-full object-cover" alt="Banner" />
+                    <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+                </div>
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+                    <h2 className='text-blue-400 text-4xl md:text-7xl font-bold'>
+                        <Typewriter words={['Search', 'Filter', 'Sort']} loop={true} cursor={true} />
+                    </h2>
+                    <h2 className="text-white text-3xl md:text-5xl font-semibold mt-4">
+                        What You Need
+                    </h2>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
+import React, { useState, useEffect } from "react";
 import Banner from "./Banner";
-
+import Featured from "./Featured";
 
 const Home = () => {
+   
+
     return (
         <div>
-            <Banner></Banner>
+            <Banner />
+            
         </div>
     );
 };
